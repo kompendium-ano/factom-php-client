497a4010e9d8029cc501ce19cd7663c0147dc1e7
Mohit Mali <mohit.mali@ftechiz.com>
2019-10-03 12:19:44 +0530

    initial comit

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+//Establishing connection with the node
+define('walletHost', 'http://localhost:8089/v2');
+define('host', 'http://localhost:8088/v2');
+?>
---
+++ src/FactomApi/ReadFactomChain.php
@@ -0,0 +1,33 @@
+<?php 
+namespace FactomApi;
+class ReadFactomChain
+{
+    // Read Chain Entry
+    public static function readChainEntry($entryHash){
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => host,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "{\n    \"jsonrpc\": \"2.0\",\n    \"id\": 0,\n    \"method\": \"entry\",\n    \"params\": {\n        \"hash\": \"".$entryHash."\"\n    }\n}",
+            CURLOPT_HTTPHEADER => array(
+                "Accept: */*",
+                "Content-Type: application/json"
+            )
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        $response = json_decode($response, true);
+
+        return $response;
+    }
+}
